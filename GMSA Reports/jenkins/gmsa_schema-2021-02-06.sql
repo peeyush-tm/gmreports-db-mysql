32fa1f5569a03db76aef76c2e6e94992274c8844
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2021-02-06 19:16:56 +0530

    update gmsa report

--- GMSA Reports/jenkins/gmsa_schema-2021-02-06.sql
+++ GMSA Reports/jenkins/gmsa_schema-2021-02-06.sql
@@ -2315,19 +2315,6 @@ CREATE TABLE IF NOT EXISTS `plan` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table gm_reports.report_mapping
-DROP TABLE IF EXISTS `report_mapping`;
-CREATE TABLE IF NOT EXISTS `report_mapping` (
-  `REPORT_ID` int(11) DEFAULT NULL,
-  `NODE_ID` int(11) DEFAULT NULL,
-  KEY `REPORT_ID` (`REPORT_ID`),
-  KEY `NODE_ID` (`NODE_ID`),
-  CONSTRAINT `NODE_ID` FOREIGN KEY (`NODE_ID`) REFERENCES `report_data_details` (`ID`),
-  CONSTRAINT `REPORT_ID` FOREIGN KEY (`REPORT_ID`) REFERENCES `reports` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='this table is used to generate the mapping between reports and respective tables  ';
-
--- Data exporting was unselected.
-
 -- Dumping structure for table gm_reports.report_metadata
 DROP TABLE IF EXISTS `report_metadata`;
 CREATE TABLE IF NOT EXISTS `report_metadata` (
@@ -2392,34 +2379,7 @@ INSERT INTO `plan` (`id`, `gm_plan_id`, `bss_plan_id`, `plan_name`) VALUES
 	(2, '2', '131', 'Plan 2');
 /*!40000 ALTER TABLE `plan` ENABLE KEYS */;
 
--- Dumping structure for table gm_reports.report_mapping
-DROP TABLE IF EXISTS `report_mapping`;
-CREATE TABLE IF NOT EXISTS `report_mapping` (
-  `REPORT_ID` int(11) DEFAULT NULL,
-  `NODE_ID` int(11) DEFAULT NULL,
-  KEY `REPORT_ID` (`REPORT_ID`),
-  KEY `NODE_ID` (`NODE_ID`),
-  CONSTRAINT `NODE_ID` FOREIGN KEY (`NODE_ID`) REFERENCES `report_data_details` (`ID`),
-  CONSTRAINT `REPORT_ID` FOREIGN KEY (`REPORT_ID`) REFERENCES `reports` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='this table is used to generate the mapping between reports and respective tables  ';
 
--- Dumping data for table gm_reports.report_mapping: ~12 rows (approximately)
-DELETE FROM `report_mapping`;
-/*!40000 ALTER TABLE `report_mapping` DISABLE KEYS */;
-INSERT INTO `report_mapping` (`REPORT_ID`, `NODE_ID`) VALUES
-	(1, 1),
-	(2, 1),
-	(3, 3),
-	(3, 6),
-	(4, 4),
-	(4, 5),
-	(4, 6),
-	(5, 7),
-	(6, 3),
-	(6, 2),
-	(8, 11),
-	(7, 9);
-/*!40000 ALTER TABLE `report_mapping` ENABLE KEYS */;
 
 -- Dumping structure for table gm_reports.cdr_data_details_vw
 DROP TABLE IF EXISTS `cdr_data_details_vw`;
